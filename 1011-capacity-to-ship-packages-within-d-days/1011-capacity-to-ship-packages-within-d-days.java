9489281a532369cdf4eb734f953ca87c126f8264
Shaurya Kapoor <shauryakapoor.work@gmail.com>
2026-07-01 18:46:50 +0530

    Time: 9 ms (98.6%), Space: 50.1 MB (56.02%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int left = 0;
+        int right = 0;
+        for (int w : weights) {
+            left = Math.max(left, w);
+            right += w;
+        }
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int need = 1;
+            int cur = 0;
+            for (int w : weights) {
+                if (cur + w > mid) {
+                    need += 1;
+                    cur = 0;
+                }
+                cur += w;
+            }
+            if (need > days) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+}
